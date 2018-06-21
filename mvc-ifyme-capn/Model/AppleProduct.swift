e56d8a4354ec2f7020f5ba09f98258f2709651b0
John Green <jpgreen@Johns-iMac.local>
2018-06-21 00:43:20 -0400

    Lesson 4

---
+++ mvc-ifyme-capn/Model/AppleProduct.swift
@@ -0,0 +1,23 @@
+//
+//  AppleProduct.swift
+//  mvc-ifyme-capn
+//
+//  Created by John Green on -06-212018.
+//  Copyright © 2018 John P. Green. All rights reserved.
+//
+
+import Foundation
+
+class AppleProduct {
+    
+    var name: String
+    var color: String
+    var price: Double
+    
+    init(name: String, color: String, price: Double) {
+        self.name = name
+        self.color = color
+        self.price = price
+    }
+    
+}
